678f1426a7a1830dc5f1d54724b1480305e78394
Mustafa Salem <mustafaasalem06@gmail.com>
2025-07-23 17:24:16 +0300

    Navbar & ResumeCard & ScoreCircle

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "Resumind"},
+        {name: "description", content: "Smart feedback for your dream job!"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Track Your applications & Resume Ratings</h1>
+                <h2>Review your submissions and check AI-powered feedback</h2>
+            </div>
+        {resumes.length > 0 && (
+            <div className="resumes-section">
+                {resumes.map((resume) => (
+                    <ResumeCard key={resume.id} resume={resume}/>
+                ))}
+            </div>
+        )}
+        </section>
+    </main>
 }
